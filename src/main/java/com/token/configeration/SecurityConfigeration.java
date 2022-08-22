76e12029cf47847ac81f36f4a82c94b3a0834141
unknown <cogjava3104@iiht.tech>
2022-08-21 22:20:42 -0700

    first commit

--- src/main/java/com/token/configeration/SecurityConfigeration.java
+++ src/main/java/com/token/configeration/SecurityConfigeration.java
@@ -8,15 +8,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.token.filter.JwtFilter;
 import com.token.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 @SuppressWarnings("deprecation")
 public class SecurityConfigeration extends WebSecurityConfigurerAdapter {
+	
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private JwtFilter jwtFilter;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -31,7 +37,10 @@ public class SecurityConfigeration extends WebSecurityConfigurerAdapter {
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
+		.anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 	
 
