597916633eb52ef30cc5d7c48a3b0eb7f67ad387
unknown <cogjava3104@iiht.tech>
2022-08-21 21:11:05 -0700

    first commit

--- src/main/java/com/token/controller/HomeController.java
+++ src/main/java/com/token/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,9 +23,10 @@ public class HomeController {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private UserService userService;
+
 	@GetMapping("/")
 	public String home() {
 		return "Welcome to JWT demo Project";
@@ -35,20 +37,19 @@ public class HomeController {
 	public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception {
 		try {
 			authenticationManager.authenticate(
-					
-		new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(),jwtRequest.getPassword()));			
-					
-					
-		}catch (BadCredentialsException e) {
-			
-			throw new Exception("INVALID CREDENTIALS",e);
-			
-			
+
+					new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
+
+		} catch (BadCredentialsException e) {
+
+			throw new Exception("INVALID CREDENTIALS", e);
+
 		}
-		
-		
-		
-		return null;
+
+		final UserDetails userDetails = userService.loadUserByUsername(jwtRequest.getUsername());
+		final String token = jwtUtility.generateToken(userDetails);
+
+		return new JwtResponse(token);
 
 	}
 
